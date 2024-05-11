466fb40b2e0e699967c8aff7996d6ec782369ead
Jeremy Chrimes <jeremy.chrimes@gmail.com>
2024-05-11 13:03:34 +0000

    Euler 1 - Test

---
+++ src/Euler1.purs
@@ -0,0 +1,16 @@
+module Euler1
+  ( answer
+  )
+  where
+
+import Prelude
+
+
+import Data.List (range, filter)
+import Data.Traversable (sum)
+
+ns = range 0 999
+
+muliples = filter (\n -> mod n 3 == 0 || mod n 5 == 0) ns
+
+answer = sum muliples
